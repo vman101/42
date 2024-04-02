7fc7b8ab17560f8be16cab25e7e0e6db1e6e1b64
vman101 <124388686+vman101@users.noreply.github.com>
2024-04-02 19:58:18 +0200

    Add files via upload

---
+++ ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vvobis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 17:42:28 by vvobis            #+#    #+#             */
+/*   Updated: 2024/04/02 18:25:37 by vvobis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*d;
+	char	*s;
+	size_t	i;
+
+	d = (char *) dest;
+	s = (char *) src;
+	if (s < d && s + n > d)
+	{
+		while (n--)
+			d[n] = s[n];
+	}
+	else if (s > d && s < d + n)
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dest);
+}
