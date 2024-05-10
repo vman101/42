c8913782028b601a84cf327e7f5a392ac2adad35
vman101 <victorvobis@web.de>
2024-05-10 07:21:42 +0000

    sifj

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+make
+
+ARG=$(cat input)
+
+if [ -z "$1" ]; then
+	./push_swap $ARG
+	exit 0
+fi
+
+case "$1" in
+	"check")
+		./push_swap $ARG | ./checker_linux $ARG
+		;;
+	"gdb")
+		gdb --args ./push_swap $ARG
+		;;
+	"val")
+		valgrind --leak-check=full ./push_swap $ARG
+esac
+
