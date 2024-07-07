febe4c1326276fe4ef1ccb47e3bc9b5c59b35147
vman101 <victorvobis@web.de>
2024-07-07 10:15:47 +0200

    minishell_start

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vvobis <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 23:43:36 by vvobis            #+#    #+#             */
-/*   Updated: 2024/06/13 15:28:43 by vvobis           ###   ########.fr       */
+/*   Updated: 2024/06/08 12:14:28 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,19 @@
 
 # define LIBFT_H
 
+# include "get_next_line.h"
+# include "ft_printf.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <stddef.h>
 # include <stdint.h>
 # include <limits.h>
-# include "ft_printf.h"
 
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
-
-/* Input Handeling */
-char	***super_split(char **strs, int size, int delimiter);
-void	*free_super_split(char ***back);
-void	free_split(char **split);
-char	*get_next_line(int fd);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -87,8 +82,7 @@ void	ft_putstr_fd(char *str, int fd);
 void	ft_putendl_fd(char *str, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
-/* safe functions */
-
-void	ft_free(void **ptr);
+/* Get Next Line */
+char	*get_next_line(int fd);
 
 #endif
